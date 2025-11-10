614ea1a72cb2660f022ba1dfb73e99e62bf5d2da
shimeuisuk <104304569+shimeeuisuk@users.noreply.github.com>
2025-11-11 00:09:45 +0900

    Time: 37 ms (55.9%), Space: 72.9 MB (39.58%) - LeetHub

--- 0015-3sum/0015-3sum.js
+++ 0015-3sum/0015-3sum.js
@@ -3,28 +3,30 @@
  * @return {number[][]}
  */
 var threeSum = function(nums) {
-    let n = nums.length
-    let answer = []
-    let temp =[]
+  const answer = []
+  nums.sort((a,b)=>a-b)
+  const n = nums.length
 
-    for (let i=0; i<n-2; i++) {
-        let standard = nums[i]
-        const target = -standard
-        const extraArray = nums.slice(i+1).sort((a,b)=>a-b)
-        let l = 0
-        let r = extraArray.length-1
-        while (l<r) {
-            const sum = extraArray[l] + extraArray[r]
-            if (sum < target) l++
-            if (sum > target) r--
-            if (sum === target) {
-                temp = [nums[i], extraArray[l], extraArray[r]]
-                temp.sort((a,b)=>a-b)
-                answer.push(temp)
-                temp = []
-                l++
-            }
+  for (let i=0; i<n-2; i++) {
+    if (i>0 && nums[i] === nums[i-1]) continue
+
+    let l = i+1
+    let r = n-1
+
+    while (l<r) {
+        const sum = nums[i] + nums[l] + nums[r]
+        if (sum === 0) {
+            answer.push([nums[i], nums[l], nums[r]])
+            while (l<r && nums[l]===nums[l+1]) l++
+            while (l<r && nums[r]===nums[r-1]) r--
+            l++
+            r--
+        } else if (sum < 0) {
+            l++
+        } else {
+            r--
         }
     }
-    return Array.from(new Set(answer.map(JSON.stringify))).map(JSON.parse);
+  }
+  return answer
 };
