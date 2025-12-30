11c59d5d280d181285a5894d4dcbb17906f6d4aa
shimeuisuk <104304569+shimeeuisuk@users.noreply.github.com>
2025-12-30 09:52:38 +0900

    Time: 8 ms (47.97%), Space: 58 MB (51.19%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -8,15 +8,14 @@ var lengthOfLongestSubstring = function(s) {
     let maxLen = 0;          
 
     for (let right = 0; right < s.length; right++) {
-        const char = s[right];
+        const char = s[right]; 
 
         // 중복이 있으면 left를 이동시키며 set에서 제거
         while (set.has(char)) {
-            set.delete(s[left]);
-            left++;
-        }
-
-        set.add(char);
+            set.delete(s[left]); 
+            left++; 
+        } 
+        set.add(char); 
 
         // 현재 윈도우 길이 갱신
         maxLen = Math.max(maxLen, right - left + 1);
