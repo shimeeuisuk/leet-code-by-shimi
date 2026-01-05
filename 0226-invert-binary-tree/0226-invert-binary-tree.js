d94dd09dc4817b1b4b0a3029c60a0bb8e239182e
shimeuisuk <104304569+shimeeuisuk@users.noreply.github.com>
2026-01-05 11:44:40 +0900

    Time: 0 ms (100%), Space: 54.6 MB (71.14%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.js
+++ 0226-invert-binary-tree/0226-invert-binary-tree.js
@@ -12,14 +12,17 @@
  */
 var invertTree = function(root) {
     if (root === null) return null
-
+    
+    // 노드 전체 트리 담아둠
     const queue = [root]
-
+    // 큐가 빌때까지 선출 할거임
     while (queue.length) {
+        // 노드 뽑아냄
         const node = queue.shift()
-
+        // 노드의 왼쪽과 오른쪽 변경
         ;[node.left, node.right] = [node.right, node.left]
 
+        // 그리고 아래로 쭉 순회 변경하고 left 쭉 순회하면서 변경, right 쭉 순회하면서 변경
         if (node.left) queue.push(node.left)
         if (node.right) queue.push(node.right)
     }
