2d0a665c888bfbb2b59770cfc6cf2e07dbaa5828
shimeuisuk <104304569+shimeeuisuk@users.noreply.github.com>
2026-01-05 15:48:29 +0900

    Time: 1 ms (2.65%), Space: 54 MB (82.46%) - LeetHub

--- 0100-same-tree/0100-same-tree.js
+++ 0100-same-tree/0100-same-tree.js
@@ -18,16 +18,18 @@ var isSameTree = function(p, q) {
         // 각 노드 선출로 뽑아냄
         const [node1, node2] = queue.shift()
 
-
+        // 둘 다 노드가 없으면 그냥 넘어감
         if (node1 === null && node2 === null) continue
-        // 둘 중 하나라도 다음 노드가 없으면
+        // 둘 중 하나라도 다음 노드가 없으면 false
         if (node1 === null || node2 === null) return false
-        // 노드의 값이 다르면 
+        // 노드의 값이 다르면 false
         if (node1.val !== node2.val) return false
 
+        // 큐에 다음 비교할 애들을 넣는다. 노드의 left와 right 
         queue.push([node1.left, node2.left])
         queue.push([node1.right, node2.right])
     }
 
+    // 다 돌았을 때 false로 안빠지면 true
     return true
 };
