a8ec7c644078d510caebaec2845a35f5ba2461f2
shimeuisuk <104304569+shimeeuisuk@users.noreply.github.com>
2026-01-13 15:17:31 +0900

    Time: 4 ms (40.24%), Space: 59.4 MB (19.32%) - LeetHub

---
+++ 0057-insert-interval/0057-insert-interval.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[][]} intervals
+ * @param {number[]} newInterval
+ * @return {number[][]}
+ */
+var insert = function(intervals, newInterval) {
+    let answer = []
+    let [newStart, newEnd] = newInterval
+
+    for (let [start, end] of intervals) {
+        if (end < newStart) {
+            answer.push([start, end])
+        } else if (start > newEnd) {
+            answer.push([start, end])
+        } else {
+            newStart = Math.min(start, newStart)
+            newEnd = Math.max(end, newEnd)
+        }
+    }
+
+    answer.push([newStart, newEnd])
+    return answer.sort((a,b)=>a[0]-b[0])
+};
