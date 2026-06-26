27fbd116be11714c079395fd5780f95d7f01e6c7
shimeuisuk <104304569+shimeeuisuk@users.noreply.github.com>
2026-06-26 15:48:15 +0900

    Time: 4 ms (51.58%), Space: 57.9 MB (5.68%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -3,11 +3,27 @@
  * @param {number} target
  * @return {number[]}
  */
+ // 브루토 포스
+// var twoSum = function(nums, target) {
+//     for (let i=0; i<nums.length; i++) {
+//         let remainNum = target - nums[i]
+//         for (let j = i+1; j < nums.length; j++) {
+//             if (remainNum === nums[j]) return [i,j]
+//         }
+//     }
+// };
+
+// 해시맵
 var twoSum = function(nums, target) {
-    for (let i=0; i<nums.length; i++) {
-        let remainNum = target - nums[i]
-        for (let j = i+1; j < nums.length; j++) {
-            if (remainNum === nums[j]) return [i,j]
+    const map = new Map()
+    for (let i = 0; i < nums.length; i++) {
+        map.set(nums[i], i)
+    }
+
+    for (let j = 0; j < nums.length; j++) {
+        const remain = target - nums[j];
+        if (map.has(remain) && map.get(remain) !== j) {
+            return [j, map.get(remain)]
         }
     }
 };
